bad55c810fad26fc92fb63b0d2b0928d393574c5
luiggy2620 <luiggymamani@gmail.com>
2024-07-04 13:44:58 -0400

    ci: absolute path added to avoid change it in different machines

--- app/src/main/java/com/tic_tac_toe/utils/SourcePaths.java
+++ app/src/main/java/com/tic_tac_toe/utils/SourcePaths.java
@@ -1,7 +1,14 @@
 package com.tic_tac_toe.utils;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class SourcePaths {
 
-    public static final String BASE_PATH = "/home/fundacion/University/Fifth/SoftwareDevelopment/TicTacToe/app/src/main/java/com/tic_tac_toe/Images";
+    public static final Path RELATIVE_APP_PATH = Paths.get("app/src/main/resources");
+
+    public static final Path ABSOLUTE_APP_PATH = RELATIVE_APP_PATH.toAbsolutePath();
+
+    public static final String BASE_PATH = ABSOLUTE_APP_PATH.toString();
 
 }
